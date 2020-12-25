cc4ab6a8ec151fb7cc56f837eaad6702190e34fb
923464541@qq.com <feifei521>
2020-12-25 10:17:34 +0800

    新增日志记录

--- main.py
+++ main.py
@@ -1,48 +1,47 @@
+from typing import Optional
 
 import random
 from threading import Lock
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
+from loguru import logger
+
 
 async_mode = None
 app = Flask(__name__)
-CORS(app, supports_credentials=True)
 
+CORS(app, supports_credentials=True)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
-
-
 close = False
 thread = None
 thread_lock = Lock()
-
-
-
-
-
+logger.add('flask.log')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def ack(data):  # 服务端回调函数
-    print('客户端已收到消息，回调参数为',data)  # 服务端回调函数的参数
+
+def ack(info: str):
+    logger.info('{} is successful'.format(info))  # 服务端回调函数的参数证明连接成功
+
 
 @socketio.on('request_for_response', namespace='/XXXprojext')
-def test_connect(data):
+def test_connect(data: dict):
     """
-    监听服务端发来的消息，判断是否能收到，然后在发送数据给服务端，
+    监听服务端发来的消息，判断是否能收到，然后在发送数据给服务端，如果回调成功说明连接没问题
     :param data:
     :return:
     """
-    res = data.get('from_server')
-    print(res)
+
+    logger.info('The client has received the message {}'.format(data))
     emit('response', {'code': 200, 'from_client': 'Success!'}, callback=ack)
 
 
 @socketio.on('stop_send_data', namespace="/XXXprojext")
-def close_tail():
+def stop_out():
     global thread, close
     close = True
     thread = None
@@ -56,17 +55,16 @@ def handle_my_custom_event():
             close = False
             thread = socketio.start_background_task(target=background_thread_task)
         else:
-            socketio.emit('tail_response', {'msg': 'loading....'}, namespace='/XXXprojext')
+            socketio.emit('tail_response', {'msg': 'Resources occupied......'}, namespace='/XXXprojext')
+
 
 def background_thread_task():
-    print('进来了')
     while not close:
         i = random.randint(0,100)
         socketio.sleep(0.1)
         socketio.emit('do_somrthing', {"num": i}, namespace='/XXXprojext')
     socketio.stop()
     return {"over"}
-
 
 
 if __name__ == '__main__':
